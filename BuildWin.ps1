e88b91d0ad8706a221d38daa6fdf3aa7e106dd33
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2025-11-28 12:59:26 +0100

    Act 15

---
+++ BuildWin.ps1
@@ -0,0 +1,5 @@
+Clear.bat
+cp main.py launcher_win.py
+python -m PyInstaller --onefile --windowed --noconsole --icon=logo.ico launcher_win.py
+python -m PyInstaller --onefile --windowed --noconsole --icon=logo.ico installer_updater.py
+echo 1.0.4 > version_win_launcher.txt
